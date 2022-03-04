0aaad58cfad32699d55f1c2fcf4659d808c95c54
PatThompson88 <patrick.thompson688@gmail.com>
2022-03-04 02:11:41 -0500

    roll-a-ball unity learn demo project

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    
+    private Vector3 offset = new Vector3(0.0f, 11.0f, -6.0f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        // offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed = 0f;
+    public TextMeshProUGUI scoreText;
+	public GameObject winTextObject;
+    public int score;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementZ;
+    void Start()
+    {
+        score = 0;
+        SetScoreText();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementZ = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0, movementZ);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // access tag element and check that it is a "Pickup"
+        if(other.gameObject.CompareTag("Pickup")){
+            other.gameObject.SetActive(false);
+            score += 50;
+            SetScoreText();
+        }
+    }
+
+    void SetScoreText()
+	{
+		scoreText.text = "SCORE: " + score.ToString();
+
+		if (score >= 250){
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+		}
+	}
+}
